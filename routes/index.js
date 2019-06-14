331109b0522c8734bdb5f32f930f1217083347f8
Kaleb Ercanbrack <kaleb@pop-os.localdomain>
2019-06-14 15:29:40 -0600

    form is made, need to create user list

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var uuidv1 = require('uuid/v1')
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "User Creation", id: uuidv1()});
 });
 
 module.exports = router;
